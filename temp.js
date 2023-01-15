ca47d964cd1176d5c418e54d64100e687ce0eb83
wakaay <2869391152@qq.com>
2023-01-15 11:10:03 +0800

    create temp.js

---
+++ temp.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Hello world
+// @namespace    http://wakaay.com/
+// @version      0.1
+// @description  try to take over the world!
+// @author       wakaay
+// @match        https://github.com/wakaay?tab=repositories
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    console.log('hello world')
+})();
