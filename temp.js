148d07a8f8d054031dff41b07e5c6d3fa6cc8e7f
wakaay <2869391152@qq.com>
2023-01-15 12:03:04 +0800

    add descripition

--- temp.js
+++ temp.js
@@ -2,7 +2,7 @@
 // @name         Hello world
 // @namespace    http://wakaay.com/
 // @version      0.1
-// @description  try to take over the world!
+// @description  print hello word
 // @author       wakaay
 // @match        https://github.com/wakaay?tab=repositories
 // @license      AGPL-3.8
