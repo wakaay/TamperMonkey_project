60385c3a7d060f9053da88a2135f9f780b102732
wakaay <2869391152@qq.com>
2023-01-15 12:00:33 +0800

    add license

--- temp.js
+++ temp.js
@@ -5,6 +5,7 @@
 // @description  try to take over the world!
 // @author       wakaay
 // @match        https://github.com/wakaay?tab=repositories
+// @license      AGPL-3.8
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
 // @grant        none
 // ==/UserScript==
